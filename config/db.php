ff72f34d670d933070e6815f6344c5b1c4235f72
jerico1990 <cesar.gago.egocheaga@gmail.com>
2016-06-24 15:13:43 -0500

    a

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mineduproyecto',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=idease8_ideasenaccion',
+    'username' => 'idease8_webuser',
+    'password' => 'c8uxaSw7wuse',
     'charset' => 'utf8',
 ];
